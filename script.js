c9b3ea333d5d83eafa3519f2a05095d16ee4d46a
laur <laura.r.brackney@gmail.com>
2024-02-19 12:05:37 -0600

    Complete RPS play round
    Fix the issues with undefined returns
    Play five games

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
-function getComputerChoice() {
-    let getComputerChoice = Math.floor(Math.random() * 3);
-    let computerSelection = (getComputerChoice === 0) ? "rock" : 
-        (getComputerChoice === 1) ? "paper" : 
+function getComputerSelection() {
+    let getComputerSelection = Math.floor(Math.random() * 3);
+    let computerSelection = (getComputerSelection === 0) ? "rock" : 
+        (getComputerSelection === 1) ? "paper" : 
         "scissors";
-    //check to make sure all 3 choices are being selected sometimes
-    // console.log("Computer chose: " + computerSelection);
     return computerSelection;
 }
 
-let computerSelection = getComputerChoice(); 
-console.log("Computer chose: " +computerSelection);
+let computerSelection; 
 
-function getPlayerSelection() {
-    let playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
-// tried to make it ask the user again if there was a typo. it did, but it didn't save the new response
-    // into the playerSelection variable and I don't know why
-    // if ((playerSelection !== "rock") && (playerSelection !== "paper") && (playerSelection !== "scissors")) {
-    //     alert("Looks like you had a typo. Please try again!");
-    //     getPlayerSelection();
-    // } else {
-        return playerSelection;
-    // }
-}
-let playerSelection = getPlayerSelection();
-console.log("Player chose: " + getPlayerSelection());
+// // function getPlayerSelection() {
+//     // if ((playerSelection !== "rock") && (playerSelection !== "paper") && (playerSelection !== "scissors")) {
+//     //     alert("Looks like you had a typo. Please try again!");
+//     //     getPlayerSelection();
+//     // } else {
+//         return playerSelection;
+
+let playerSelection;
 
 function playRound(playerSelection, computerSelection) {
-   
+    playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+    console.log("Player chose: " + playerSelection);
+    computerSelection = getComputerSelection(); 
+    console.log("Computer chose: " + computerSelection);
     if (playerSelection === computerSelection) {
-        console.log("comp picked: " + computerSelection + "and u picked: " + playerSelection);
         console.log("It's a tie!");
     } else if ((playerSelection === "rock") && (computerSelection == "scissors")) {
         console.log("You win! Rock beats scissors.");
@@ -41,4 +35,12 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-playRound();
+function playGame() {
+    playRound(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
+}
+
+playGame();
