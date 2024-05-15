81d72f28c2b49fe924f3aa3a946e21f94a579c86
laur <laura.r.brackney@gmail.com>
2024-05-15 17:34:38 -0500

    Resolve logic issue and display choices made

--- script.js
+++ script.js
@@ -17,24 +17,82 @@ let computerSelection;
 
 let playerSelection;
 
+const choices = document.querySelector('#choices');
+choices.addEventListener("click", (event) => {
+    let choice = event.target;
+
+    switch(choice.id) {
+        case "rock": 
+            playerSelection = "rock";
+            // console.log(playerSelection);
+            break;
+            // return playerSelection;
+        case "paper":
+            playerSelection = "paper";
+            // console.log(playerSelection);
+            break;
+        case "scissors":
+            playerSelection = "scissors";
+            // console.log(playerSelection);
+            break;
+    }
+    playRound(playerSelection, computerSelection);
+});
+
 function playRound(playerSelection, computerSelection) {
     // playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
-    console.log("Player chose: " + playerSelection);
+    const body = document.querySelector("body");
+    const div = document.createElement("div");
+    div.style.border = "solid rgb(179, 250, 199) 2px";
+    div.style.marginTop = "5px";
+    
+    body.appendChild(div);
+    
+    const playerChoice = document.createElement("p");
+    playerChoice.textContent = "Player chose: " + playerSelection; 
+    div.appendChild(playerChoice);
+    
+    const compChoice = document.createElement("p");
     computerSelection = getComputerSelection(); 
-    console.log("Computer chose: " + computerSelection);
-   
-   // all this has to be changed!
+    compChoice.textContent = "Computer chose: " + computerSelection;
+    div.appendChild(compChoice);
+
+    
+    const results = document.createElement("p");
+    // trying to remove the previous added "p" node's textcontent
+    // if (choices !== "") {
+    //     choices.removeChild(results);
+    // }
     if (playerSelection === computerSelection) {
-        console.log("It's a tie!");
+        results.textContent = "It's a tie!";
+        choices.appendChild(results);
     } else if ((playerSelection === "rock") && (computerSelection == "scissors")) {
-        console.log("You win! Rock beats scissors.");
+        results.textContent = "You win! Rock beats scissors.";
+        choices.appendChild(results);
     } else if ((playerSelection === "paper") && (computerSelection == "rock")) {
-        console.log("You win! Paper beats rock.");
+        results.textContent = "You win! Paper beats rock.";
+        choices.appendChild(results);
     } else if ((playerSelection === "scissors") && (computerSelection == "paper")) {
-        console.log("You win! Scissors beats paper.");
+        results.textContent = "You win! Scissors beats paper.";
+        choices.appendChild(results);
     } else {
-        console.log("You lose!");
+        results.textContent = "You lose!";
+        choices.appendChild(results);
     }
+    
+
+   // all this has to be changed!
+    // if (playerSelection === computerSelection) {
+    //     console.log("It's a tie!");
+    // } else if ((playerSelection === "rock") && (computerSelection == "scissors")) {
+    //     console.log("You win! Rock beats scissors.");
+    // } else if ((playerSelection === "paper") && (computerSelection == "rock")) {
+    //     console.log("You win! Paper beats rock.");
+    // } else if ((playerSelection === "scissors") && (computerSelection == "paper")) {
+    //     console.log("You win! Scissors beats paper.");
+    // } else {
+    //     console.log("You lose!");
+    // }
 
     // removing console.logs ... reworking to changing textContents or nodes / appendages
 }
@@ -52,24 +110,7 @@ function playRound(playerSelection, computerSelection) {
 // not sure about this ... 
 // const rockBtn = document.querySelector("rock")
 
-const choices = document.querySelector('#choices');
-
-choices.addEventListener("click", (event) => {
-    let choice = event.target;
-
-    switch(choice.id) {
-        case "rock": 
-            playerSelection = "rock";
-            break;
-        case "paper":
-            playerSelection = "paper";
-            break;
-        case "scissors":
-            playerSelection = "scissors";
-            break;
-    }
-    playRound(playerSelection, computerSelection);
-});
+// js update: 
 
 
 
